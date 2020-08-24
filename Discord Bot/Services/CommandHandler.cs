5a76e025aa4c8ca11241d957bd0283eacc116c18
Brandon Bednarek <brandon@bednarek.ca>
2020-08-24 19:36:31 -0400

    Only let bot read from bot specific text channels

--- Discord Bot/Services/CommandHandler.cs
+++ Discord Bot/Services/CommandHandler.cs
@@ -26,33 +26,33 @@ namespace Discord_Bot
 
             // checks if message is a link
             var link = new Regex(@"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)");
-            if (link.IsMatch(message.Content))
-            {
-                return;
-            }
 
             // Converts Celsius in messages to Fahrenheit
-            var regex = new Regex(@"-?\d+(?:\.\d+)?(?:\s*°)?[cCfF]");
-            if (regex.IsMatch(message.Content))
+            var temp = new Regex(@"-?\d+(?:\.\d+)?(?:\s*°)?[cCfF]");
+
+            if (!link.IsMatch(message.Content))
             {
-                var matches = regex.Matches(message.Content);
-                try
+                if (temp.IsMatch(message.Content))
                 {
-                    string num = matches[0].Value.ToUpper().Replace(@"C", string.Empty);
+                    var matches = temp.Matches(message.Content);
+                    try
+                    {
+                        string num = matches[0].Value.ToUpper().Replace(@"C", string.Empty);
 
-                    int celsius = Int32.Parse(num);
-                    int fahrenheit = (celsius * 9) / 5 + 32;
+                        int celsius = Int32.Parse(num);
+                        int fahrenheit = (celsius * 9) / 5 + 32;
 
-                    await message.Channel.SendMessageAsync(num + "°C / " + fahrenheit + "°F");
-                }
-                catch
-                {
-                    string num = matches[0].Value.ToUpper().Replace(@"F", string.Empty);
+                        await message.Channel.SendMessageAsync(num + "°C / " + fahrenheit + "°F");
+                    }
+                    catch
+                    {
+                        string num = matches[0].Value.ToUpper().Replace(@"F", string.Empty);
 
-                    int fahrenheit = Int32.Parse(num);
-                    int celsius = (fahrenheit - 32) * 5 / 9;
+                        int fahrenheit = Int32.Parse(num);
+                        int celsius = (fahrenheit - 32) * 5 / 9;
 
-                    await message.Channel.SendMessageAsync(num + "°F / " + celsius + "°C");
+                        await message.Channel.SendMessageAsync(num + "°F / " + celsius + "°C");
+                    }
                 }
             }
 
@@ -70,27 +70,40 @@ namespace Discord_Bot
             if (message.Content[0] != '!')
                 return;
 
-
-            // Bot replies to !ping with !pong
-            if (message.Content == "!ping")
+            // The bot should only read messages from #bot-commands, else delete
+            if (message.Channel.Id != 747591343546695710 && message.Channel.Id != 694062245566611517)
             {
-                await message.Channel.SendMessageAsync("pong!"); return;
+                await (message.Channel as SocketTextChannel).DeleteMessageAsync(message);
+                return;
             }
 
-            if (message.Content.Contains("!clear") && message.Author.Id == 147163300818321408)
-            {
-                try
-                {
-                    int toDelete = Int32.Parse(message.Content.Remove(0, 6).Trim());
-                    var messages = await message.Channel.GetMessagesAsync(toDelete + 1).FlattenAsync(); //defualt is 100
-                    await (message.Channel as SocketTextChannel).DeleteMessagesAsync(messages);
-                    return;
-                }
-                catch
-                {
-                    return;
-                }
-            }
+
+            //// Bot replies to !ping with !pong
+            //if (message.Content == "!ping")
+            //{
+            //    await message.Channel.SendMessageAsync("pong!"); return;
+            //}
+
+            //// Bot replies to !ping with !pong
+            //if (message.Content == "!pingp")
+            //{
+            //    await message.Channel.SendMessageAsync("pong!"); return;
+            //}
+
+            //if (message.Content.Contains("!clear") && message.Author.Id == 147163300818321408)
+            //{
+            //    try
+            //    {
+            //        int toDelete = Int32.Parse(message.Content.Remove(0, 6).Trim());
+            //        var messages = await message.Channel.GetMessagesAsync(toDelete + 1).FlattenAsync(); //defualt is 100
+            //        await (message.Channel as SocketTextChannel).DeleteMessagesAsync(messages);
+            //        return;
+            //    }
+            //    catch
+            //    {
+            //        return;
+            //    }
+            //}
 
             if (message.Content == "!watch")
             {
